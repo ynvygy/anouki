d29c57878b91814916b3cbaa6265e433e1a229a9
ynvygy <lugerand2004@yahoo.com>
2015-10-13 09:00:34 +0300

    Buttons update

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root "welcome#index"
+  get '/about', to: "pages#about"
+  resources :posts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150901125156) do
+ActiveRecord::Schema.define(version: 20150901141105) do
+
+  create_table "posts", force: true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
